cf4f9d86db20c65fc88698dd0c6e159a6ce55439
Elena <olena168@gmail.com>
2023-02-18 22:06:22 +0300

    change video again

--- src/NewRecipe.js
+++ src/NewRecipe.js
@@ -3,7 +3,7 @@ function NewRecipe ({anyRecipe}) {
         <div>
             <div className="newRecipe">
                 <div className="title">
-                    <h3>{anyRecipe.recipe.label.lenght < 10 ? anyRecipe.recipe.label : anyRecipe.recipe.label.substring(0,30)}</h3>
+                    <h3>{anyRecipe.recipe.label.lenght < 10 ? anyRecipe.recipe.label : anyRecipe.recipe.label.substring(0,15)}</h3>
                 </div>
                 <img src={anyRecipe.recipe.image} width='300px' height='300px' alt='pic'/>
             </div>
